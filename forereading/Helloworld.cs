ede148dbf760630877540a828b739e56e9a711f2
TigeZ <lass043g@zbc.dk>
2025-09-08 15:08:13 +0200

    I forgot i was on a different branch lol

---
+++ forereading/Helloworld.cs
@@ -0,0 +1,11 @@
+using System; 
+
+    public class Geeks { 
+        
+        public static void Main(string[] args) { 
+            
+            // statement printing Hello World! 
+            Console.WriteLine("Hello World!"); 
+            
+        } 
+    } 
